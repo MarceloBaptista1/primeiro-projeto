12ecd41b08d895a700d61f156a05027b0abe3096
MarceloBaptista1 <marcelohenriquebaptista1@gmail.com>
2025-09-09 21:42:14 -0300

    Update all

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -1,5 +1,6 @@
 import { use, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import "./filme-info.css";
 import api from "../../services/api";
 
 function Filme() {
@@ -52,6 +53,13 @@ function Filme() {
             <span>{filme.overview}</span>
 
             <strong>Avaliações: {filme.vote_average} /10</strong>
+
+            <div className="area-buttons">
+                <button>Salvar</button>
+                <button>
+                    <a href="#">Trailer</a>
+                </button>
+            </div>
         </div>
     );
 }
